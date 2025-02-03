484961ccba8fdcf615f76f1fd87b31bc46a881f3
Klyarchik <zakvon79.104@gmail.com>
2025-02-03 20:45:24 +0300

    03.02 20:45

---
+++ frontend/static/reg.js
@@ -0,0 +1,39 @@
+async function getResponse() {
+   
+    if (!document.querySelector('#login_reg').value || 
+        !document.querySelector('#password_reg').value || 
+        !document.querySelector('#name_reg').value ||
+        !document.querySelector('#surname_reg').value ) {
+        alert('Пожалуйста, введите все данные.');
+        return;
+    }
+
+    let response = await fetch('http://212.22.82.237:8000/registration', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            login: document.querySelector('#login_reg').value,
+            password: document.querySelector('#password_reg').value,
+            name: document.querySelector('#name_reg').value,
+            surname: document.querySelector('#surname_reg').value
+        })
+    })
+
+    if (response.ok) {
+        const data = await response.json()
+        alert('Успешный вход: ', data)
+        localStorage.setItem('token', data.token);
+        console.log(data)
+        window.location.href='privet.html'
+
+    } else {
+        alert('Неправильный логин или пароль')
+        console.log('yyy')
+    }
+
+    
+
+}
+
+
+document.querySelector('#submit').onclick = getResponse
---
+++ frontend/static/vhod.js
@@ -0,0 +1,34 @@
+async function setResponse() {
+
+    const log = document.querySelector('#login_vhod').value
+    const pass = document.querySelector('#password_vhod').value
+
+    if (!log || !pass) {
+        alert('Пожалуйста, введите логин и пароль.');
+        return;
+    }
+
+    let response = await fetch('http://212.22.82.237:8000/enter', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            login: log,
+            password: pass
+        })
+    })
+
+    if (response.ok) {
+        const data = await response.json()
+        console.log('ttt')
+        alert('Успешный вход: ', data)
+        localStorage.setItem('token', data.token);
+        console.log(data)
+        window.location.href='privet.html'
+    } else {
+        alert('Неправильный логин или пароль')
+        console.log('yyy')
+    }
+
+}
+
+document.querySelector('#button_vhod').onclick = setResponse
