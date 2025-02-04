e931a3d4df8ddafe41971330610355978a010cd4
Klyarchik <zakvon79.104@gmail.com>
2025-02-04 20:57:47 +0300

    04.02 20:57

--- frontend/static/route.js
+++ frontend/static/route.js
@@ -1,17 +1,3 @@
-async function exit() {
-    
-    let response = await fetch('http://127.0.0.1:8000/exit', {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'}
-    })
-
-    window.location.href='glav_str.html'
-    
-}
-
-document.querySelector('#button_glav_exit').onclick = exit
-
-
 let myMap;
 let routePoints = [];
 
