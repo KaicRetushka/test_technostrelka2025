c731650fead930f66ad171c5395ff51dc5e7a744
Klyarchik <zakvon79.104@gmail.com>
2025-02-06 11:00:49 +0300

    06.02 11:00

--- frontend/static/glav_str.js
+++ frontend/static/glav_str.js
@@ -1,16 +1,35 @@
-const btn_add_polyline = document.getElementById('btn_add_polyline')
+const btn_add_polyline = document.getElementById('btn_add_route')
 console.log(btn_add_polyline)
 let is_polyline = false
+
+
 
 ymaps.ready(init);
 function init(){
     let myMap = new ymaps.Map("map", {
-    center: [55.76, 37.64],
-    zoom: 16
+        center: [55.76, 37.64],
+        zoom: 16
     });
-      
-
-    let myPolyline = new ymaps.Polyline([]);
+    
+    let myPolyline = new ymaps.Polyline([], {}, {
+        strokeColor: "#00000088",
+        // Ширину линии.
+        strokeWidth: 4,
+    
+        // Добавляем в контекстное меню новый пункт, позволяющий удалить ломаную.
+        editorMenuManager: function (items) {
+            items.push({
+                title: "Удалить линию",
+                onClick: function () {
+                    // myMap.geoObjects.remove(myPolyline);
+                    arr = []
+                    myPolyline.geometry.setCoordinates(arr)
+                    is_polyline = false
+                }
+            });
+            return items;
+        }
+    });
     myMap.geoObjects.add(myPolyline);
     let arr = []
     myMap.events.add('click', (event) => {
@@ -20,31 +39,47 @@ function init(){
         myPolyline.geometry.setCoordinates(arr)
     }
     });
-    myMap.events.add('contextmenu', (event) => {
-    myMap.balloon.open(event.get('coords'), 'меню')
-    });
+    myPolyline.editor.startEditing();
 }
 
 btn_add_polyline.addEventListener('click', () => {
-  console.log('click')
+  console.log('Было нажатие')
   is_polyline = true
 })
 
 async function exit() {
-    
     let response = await fetch('http://127.0.0.1:8000/exit', {
         method: 'DELETE',
         headers: {'Content-Type': 'application/json'}
     })
 
-    window.location.href='glav_str.html'
-    
+    window.location.href='glav_str.html'  
 }
 
 document.querySelector('#button_glav_exit').onclick = exit
 
+<<<<<<< Updated upstream
 function go_lich_kab(){
     window.location.href = 'http://127.0.0.1:8000/lich_kab.html'
 }
 
 document.querySelector('#fullname').onclick = go_lich_kab
+=======
+
+let okno = document.querySelector('#okno')
+
+let button_save_route = document.querySelector('#btn_save_route')
+button_save_route.addEventListener('click', () => {
+    okno.showModal()
+});
+
+let button_cancel = document.querySelector('#button_cancel')
+button_cancel.addEventListener('click', () => {
+    okno.close()
+});
+
+let button_close = document.querySelector('#btnclose')
+button_cancel.addEventListener('click', () => {
+    okno.close()
+});
+>>>>>>> Stashed changes
